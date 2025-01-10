10daae3496971bf71a25654acebe249745b2d4b3
Paul-Hartman <paul.hartman222@gmail.com>
2025-01-10 18:02:28 +0100

    working ex02

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -2,12 +2,14 @@
 #include <ctime>
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
+
 
 int	Account::getNbAccounts( void )
 {
@@ -54,26 +56,58 @@ Account::Account( void ) {};
 
 Account::Account( int initial_deposit )
 {
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
 	_amount = initial_deposit;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";"
+	<< "amount:" << _amount << ";"
+	<< "created" << std::endl;
 }
 
-Account::~Account( void ) {};
+Account::~Account( void ) 
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";"
+	<< "amount:" << _amount << ";"
+	<< "closed" << std::endl;
+}
 
 void	Account::makeDeposit( int deposit )
 {
 	_amount += deposit;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
 	_nbDeposits++;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";"
+	<< "p_amount:" << _amount - deposit << ";"
+	<< "deposit:" << deposit << ";"
+	<< "amount:" << _amount << ";"
+	<< "nb_deposits:" << _nbDeposits << std::endl;
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
 	if (_amount < withdrawal)
-		return (false);
-	else
 	{
-		_amount -= withdrawal;
-		_nbWithdrawals++;
+		_displayTimestamp();
+		std::cout << "index:" << _accountIndex << ";"
+		<< "p_amount:" << _amount << ";"
+		<< "withdrawal:refused" << std::endl;
+		return (false);
 	}
+	_amount -= withdrawal;
+	_nbWithdrawals++;
+	_totalNbWithdrawals++;
+	_totalAmount -= withdrawal;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";"
+	<< "p_amount:" << _amount + withdrawal << ";"
+	<< "withdrawal:" << withdrawal << ";"
+	<< "amount:" << _amount << ";"
+	<< "nb_withdrawals:" << _nbWithdrawals << std::endl;
 	return (true);
 }
 
